afad83c59e42337984a74adb24c36d003876aed7
James Fu <fzjames2037@gmail.com>
2021-08-21 18:12:07 -0600

    [NF] working on parsing.

---
+++ src/gmail.js
@@ -0,0 +1,148 @@
+
+import * as fs from "fs"
+import * as readline from "readline"
+import {google} from "googleapis"
+
+// If modifying these scopes, delete token.json.
+const SCOPES = ["https://www.googleapis.com/auth/gmail.readonly"];
+// The file token.json stores the user"s access and refresh tokens, and is
+// created automatically when the authorization flow completes for the first
+// time.
+
+const TOKEN_PATH = "token.json"
+const CRED_PATH = "credentials.json"
+
+
+/*
+// Load client secrets from a local file.
+fs.readFile(CRED_PATH, (err, content) => {
+  if (err) return console.log("Error loading client secret file:", err);
+  // Authorize a client with credentials, then call the Gmail API.
+  authorize(JSON.parse(content), listLabels);
+});
+*/
+
+/**
+ * Create an OAuth2 client with the given credentials, and then execute the
+ * given callback function.
+ * @param {Object} credentials The authorization client credentials.
+ * @param {function} callback The callback to call with the authorized client.
+ */
+
+
+function readCredentials() {
+    return new Promise((resolve, reject) => {
+        fs.readFile(CRED_PATH, (err, content) => {
+          if (err) reject(`Error loading client secret file: ${err}`);
+          resolve(JSON.parse(content))
+        });
+    })
+}
+
+
+function authorize() {
+    return new Promise((resolve, reject) => {
+        readCredentials()
+            .then(creds => {
+                const {client_secret, client_id, redirect_uris} = creds.installed;
+                const service = new google.auth.OAuth2(
+                    client_id, client_secret, redirect_uris[0]);
+
+                fs.readFile(TOKEN_PATH, (err, token) => {
+                    if (err) {
+                        getNewToken(service)
+                            .then(res => resolve(res))  
+                            .catch(err => reject(err))
+                    } else {
+                        service.setCredentials(JSON.parse(token))
+                        resolve(service)
+                    }
+                });
+            }).catch(err => reject(err))
+    })
+}
+
+
+/**
+ * Get and store new token after prompting for user authorization, and then
+ * execute the given callback with the authorized OAuth2 client.
+ * @param {google.auth.OAuth2} oAuth2Client The OAuth2 client to get token for.
+ * @param {getEventsCallback} callback The callback for the authorized client.
+ */
+function getNewToken(service) {
+    return new Promise((resolve, reject) => {
+        const authUrl = service.generateAuthUrl({
+            access_type: "offline",
+            scope: SCOPES,
+        });
+        console.log("Authorize this app by visiting this url:", authUrl);
+        const rl = readline.createInterface({
+            input: process.stdin,
+            output: process.stdout,
+        });
+        rl.question("Enter the code from that page here: ", (code) => {
+            rl.close()
+            service.getToken(code, (err, token) => {
+                if (err) reject(`Error retrieving access token ${err}`);
+                service.setCredentials(token)
+                // Store the token to disk for later program executions
+                fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
+                    if (err) reject(err)
+                    console.log("Token stored to", TOKEN_PATH)
+                });
+                resolve(service)
+            });
+        })
+    })
+}
+
+
+/*
+export default {
+    use(callback) {
+        if (service != undefined) {
+
+        }
+    }
+}
+*/
+
+/*
+async function listLabels() {
+    const auth = await authorize()
+    const gmail = google.gmail({version: 'v1', auth});
+    gmail.users.labels.list({
+        userId: 'me',
+    }, (err, res) => {
+        if (err) return console.log('The API returned an error: ' + err);
+        const labels = res.data.labels;
+        if (labels.length) {
+            console.log('Labels:');
+            labels.forEach((label) => {
+                console.log(`- ${label.name}`);
+            });
+        } else {
+            console.log('No labels found.');
+        }
+    });
+}
+
+listLabels()
+*/
+
+let service
+
+export default {
+    getService: function() {
+        return new Promise(async (resolve, reject) => {
+            if (service == undefined) {
+                const auth = await authorize()
+                service = google.gmail({version: "v1", auth: auth})
+            }
+            resolve(service)
+        })
+    }
+}
+
+
+
--- src/index.js
+++ src/index.js
@@ -1,12 +1,133 @@
 
-const { Client, Intents } = require("discord.js");
+import cron from "node-cron"
+import firebase from "firebase-admin"
+import { Client, Intents } from "discord.js"
+import { readFile } from "fs/promises"
+
+import gmail from "./gmail.js"
+
+const SACC_PATH = "serviceAccount.json"
+
+const serviceAccount = await readFile(SACC_PATH)
+                                .then(contents => JSON.parse(contents))
 
 
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] })
+
+firebase.initializeApp({
+    credential: firebase.credential.cert(serviceAccount)
+})
+        
+const db = firebase.firestore()
+
+
+
+class Registered {
+    constructor(ID, Name, Guardian, Instrument, 
+                Platform, Email, Phone, Prefers, 
+                From, Statement, join_on, ref_ID) {
+        this.ID = ID
+        this.Name = Name
+        this.Guardian = Guardian
+        this.Instrument = Instrument
+        this.Platform = Platform
+        this.Email = Email
+        this.Phone = Phone
+        this.Prefers
+    }
+}
+
+
+class Email {
+    constructor(subject, body) {
+        this.subject = subject
+        this.body = body
+    }
+
+    toRegistered() {
+        const vals = this.body.split("<br><br>")
+        vals.pop()
+        console.log(vals)
+
+        const obj = {}
+        const re = /\<strong\>([\w|\s]+)\<\/strong\>\:\s(.+)/g
+        for (let item of vals) {
+            const res = item.match(re)
+            if (res == null) {
+                console.log(item, "is causing a null!")
+            } else {
+                obj[res[0]] = res[1]
+            }
+        }
+    
+        console.log("Finished!")
+        console.log(obj)
+    }
+}
+
+
+
+function parseEmails(emailIds) {
+    return new Promise((resolve, reject) => {
+        gmail.getService().then(async service => {
+            const filtered = []
+            for (let id of emailIds) {
+                const res = await service.users.messages.get({
+                    "userId": "me",
+                    "id": id.id,
+                })
+
+                if (res.status == 200) {
+                    for (let header of res.data.payload.headers) {
+                        if (header.name == "Subject" && header.value == "Automatic Student Registration") {
+                            const buff = Buffer.from(res.data.payload.body.data, "base64")
+                            const body = buff.toString("utf-8")
+                            const subject = header.value
+                            filtered.push(new Email(subject, body))
+                            break
+                        }
+                    }
+                } else {
+                    console.error(res)
+                }
+            }
+            resolve(filtered)
+        }).catch(err => reject(err))
+    })
+}
+
+
+function checkGmail() {
+    gmail.getService().then(async service => {
+        const res = await service.users.messages.list({
+            q: `in:inbox is:read`,
+            userId: 'me',
+        });
+    
+        if (res.status == 200) {
+            if (res.data.resultSizeEstimate > 0) {
+                const emails = await parseEmails(res.data.messages)
+                console.log(emails[0].toRegistered())
+            } else {
+                console.log(`No unread messages.`)
+            }
+        } else {
+            console.error(res)
+        }
+    })
+}
+
 
 client.once("ready", () => {
-    console.log("Ready!");
+    console.log("Ready!")
+
+    const task = cron.schedule("*/2 * * * * *", () => {
+        console.log("CRON JOB")
+        checkGmail();
+    })
+    console.log("CRONs initialized")
+    task.start()
 });
 
-client.login("ODUzMDE3Mzk4MTYzOTMxMTQ2.YMPQXA.4w7jFGOsfHT-dZ4FrtF9FOz46Wo");
+client.login("ODUzMDE3Mzk4MTYzOTMxMTQ2.YMPQXA.4w7jFGOsfHT-dZ4FrtF9FOz46Wo")
 
